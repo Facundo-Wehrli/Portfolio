e4662eebedb3197f1ddaf1be58cc31debc661626
Facundo <wehrlifacundo9@gmail.com>
2023-05-11 22:15:00 -0300

    add: header & nav

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,34 @@
 /*=============== SHOW MENU ===============*/
+const navMenu = document.getElementById('nav-menu'),
+    navToggle = document.getElementById('nav-toggle'),
+    navClose = document.getElementById('nav-close')
+/*=============== MENU SHOW ===============*/
+// validate if constant exists
+if (navToggle) {
+    navToggle.addEventListener('click', () => {
+        navMenu.classList.add('show-menu')
+    })
+}
+
+/*=============== MENU HIDDEN ===============*/
+// validate if constant exists
+if (navClose) {
+    navClose.addEventListener('click', () => {
+        navMenu.classList.remove('show-menu')
+    })
+}
+
+// validate if constant exists 
 
 
 /*=============== REMOVE MENU MOBILE ===============*/
-
+const navLink = document.querySelectorAll('.nav__link')
+const linkAction = () => {
+    const navMenu = document.getElementById('nav-menu')
+    // When we click on each nav__link, we remove the show-menu class
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
 
 /*=============== ADD BLUR TO HEADER ===============*/
 
@@ -10,7 +36,7 @@
 /*=============== EMAIL JS ===============*/
 
 
-/*=============== SHOW SCROLL UP ===============*/ 
+/*=============== SHOW SCROLL UP ===============*/
 
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
