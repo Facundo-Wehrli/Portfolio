7a618c9bd924301aac4a34d35f37edf06884f255
Facundo <wehrlifacundo9@gmail.com>
2023-09-05 09:33:12 -0300

    feat: add blur to header

--- assets/js/main.js
+++ assets/js/main.js
@@ -31,7 +31,12 @@ const linkAction = () => {
 navLink.forEach(n => n.addEventListener('click', linkAction))
 
 /*=============== ADD BLUR TO HEADER ===============*/
-
+const scrollHeader = () => {
+    const header = document.getElementById('header')
+    //When the scroll is greaterthan 50 viewport height, add the scroll-header class to the header tag
+    this.scrollY >= 50 ? header.classList.add('scroll-header') : header.classList.remove('scroll-header')
+}
+window.addEventListener('scroll', scrollHeader)
 
 /*=============== EMAIL JS ===============*/
 
