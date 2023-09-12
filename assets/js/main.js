85eef1e91b1e8c5adf1d0117a389fe0e00483b4b
Facundo <wehrlifacundo9@gmail.com>
2023-09-11 22:20:55 -0300

    chore: delete docs

--- assets/js/main.js
+++ assets/js/main.js
@@ -33,7 +33,6 @@ navLink.forEach(n => n.addEventListener('click', linkAction))
 /*=============== ADD BLUR TO HEADER ===============*/
 const blurHeader = () => {
     const header = document.getElementById('header')
-    //When the scroll is greaterthan 50 viewport height, add the blur-header class to the header tag
     this.scrollY >= 50 ? header.classList.add('blur-header') : header.classList.remove('blur-header')
 }
 window.addEventListener('scroll', blurHeader)
